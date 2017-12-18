43e315e595cf6ce79a34c2c94b27e86813fd75b9
Eduardo Ferreira <camponez@gmail.com>
2017-12-18 13:05:58 +0000

    Apply autopep8

--- mercadobitcoin.py
+++ mercadobitcoin.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
+"""
+Module mercado bitcoin
+"""
 
 import datetime
 import json
 import sys
 import time
 import urllib2
-import pprint
 
-date = int(time.mktime(time.strptime(sys.argv[1], "%Y-%m-%d")))
-url = "https://www.mercadobitcoin.net/api/BTC/trades/%d/%d/" % (date, date+90000)
+DATE = int(time.mktime(time.strptime(sys.argv[1], "%Y-%m-%d")))
+URL = "https://www.mercadobitcoin.net/api/BTC/trades/%d/%d/" % (DATE,
+                                                                DATE + 90000)
 
 # print url
-response = urllib2.urlopen(url)
-data = json.loads(response.read())
+RESPONSE = urllib2.urlopen(URL)
+DATA = json.loads(RESPONSE.read())
 
-if len(data) > 0:
-    out_date = datetime.datetime.fromtimestamp(data[0]["date"]).strftime("%Y-%m-%d")
-    out_price = float(data[0]["price"])
-    print "P %s BTC %6.0f BRL" % (out_date, out_price)
+if len(DATA) > 0:
+    OUT_DATE = datetime.datetime.fromtimestamp(
+        DATA[0]["date"]).strftime("%Y-%m-%d")
+    OUT_PRICE = float(DATA[0]["price"])
+    print "P %s BTC %6.0f BRL" % (OUT_DATE, OUT_PRICE)
