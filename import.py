5e7d68b5ce2bb13e51c6f721b7bc070052373715
Paulo Flabiano Smorigo <pfsmorigo@gmail.com>
2018-01-23 18:29:15 -0200

    import.py: now using argparse

--- import.py
+++ import.py
@@ -6,42 +6,43 @@ Module import
 import sys
 import os
 import configparser
+import argparse
 
 from ledger import load_list_csv
 from ledger import Itau
 from ledger import Nubank
 from ledger import QIF
 
-if len(sys.argv) < 3:
-    print(
-        "Need arguments! {} <account> <input_file> <ledger_file>".format(
-            sys.argv[0]))
-else:
-    CONFIG = configparser.ConfigParser()
-    CONF_ACCOUNT = None
-    LEDGER_FILE = None
+parser = argparse.ArgumentParser(
+        description="Import files into ledger format.")
+parser.add_argument("-d", type=str, metavar="DATE", dest="date",
+        help="Start parsing from this date")
+parser.add_argument("-o", type=argparse.FileType("w"), default=None,
+        dest="output", metavar="OUTPUT",
+        help="Output file to write new entries")
+parser.add_argument("account",
+        help="Format name. Currently supports: 'itau', 'nubank', 'qif'")
+parser.add_argument("input_file", type=argparse.FileType('r'),
+        help="Input file used by the parser")
+args = parser.parse_args()
 
-    load_list_csv()
-    ACCOUNT = sys.argv[1]
+CONFIG = configparser.ConfigParser()
+CONF_ACCOUNT = None
 
-    if os.path.isfile('ledgerutils.conf'):
-        CONFIG.read('ledgerutils.conf')
+load_list_csv()
 
-    if ACCOUNT in CONFIG.sections():
-        CONF_ACCOUNT = CONFIG[ACCOUNT]
+if os.path.isfile('ledgerutils.conf'):
+    CONFIG.read('ledgerutils.conf')
 
-    if len(sys.argv) > 3:
-        LEDGER_FILE = sys.argv[3] if os.path.isfile(sys.argv[3]) else None
+if args.account in CONFIG.sections():
+    CONF_ACCOUNT = CONFIG[args.account]
 
-    LIST_BANKS = {
-        'itau': Itau(LEDGER_FILE, CONF_ACCOUNT),
-        'nubank': Nubank(LEDGER_FILE, CONF_ACCOUNT),
-        'qif': QIF(LEDGER_FILE, CONF_ACCOUNT)
-    }
+LIST_BANKS = {
+    'itau': Itau(args.output, CONF_ACCOUNT),
+    'nubank': Nubank(args.output, CONF_ACCOUNT),
+    'qif': QIF(args.output, CONF_ACCOUNT)
+}
 
-    BANK = LIST_BANKS[ACCOUNT]
-
-    with open(sys.argv[2], 'r') as f:
-        BANK.read_file(f)
-        f.close()
-    BANK.write_entry()
+BANK = LIST_BANKS[args.account]
+BANK.read_file(args.input_file)
+BANK.write_entry()
