33d843944bede38635892f46558e9f55f01b42d8
Paulo Flabiano Smorigo <pfsmorigo@gmail.com>
2018-01-23 21:07:38 -0200

    import.py: input now is optional
    ...because it can be defined inside the config file

--- import.py
+++ import.py
@@ -18,18 +18,20 @@ parser = argparse.ArgumentParser(
         description="Import files into ledger format.")
 parser.add_argument("-d", type=str, metavar="DATE", dest="date",
         help="Start parsing from this date")
+parser.add_argument("-i", type=argparse.FileType("r"), default=None,
+        dest="input_file", metavar="INPUT",
+        help="Input file used by the parser")
 parser.add_argument("-o", type=argparse.FileType("w"), default=None,
-        dest="output", metavar="OUTPUT",
+        dest="output_file", metavar="OUTPUT",
         help="Output file to write new entries")
 parser.add_argument("account",
         help="Format name. Currently supports: 'itau', 'nubank', 'qif'")
-parser.add_argument("input_file", type=argparse.FileType('r'),
-        help="Input file used by the parser")
 args = parser.parse_args()
 
 CONFIG = configparser.ConfigParser()
-conf_account = None
+conf = None
 from_date = None
+input_file = None
 
 load_list_csv()
 
@@ -40,14 +42,21 @@ if os.path.isfile('ledgerutils.conf'):
     CONFIG.read('ledgerutils.conf')
 
 if args.account in CONFIG.sections():
-    conf_account = CONFIG[args.account]
+    conf = CONFIG[args.account]
+
+# Command line has priority over config file
+if args.input_file:
+    input_file = args.input_file
+elif 'input_file' in conf:
+    input_file = open(conf['input_file'], "r")
 
 LIST_BANKS = {
-    'itau': Itau(args.output, conf_account, from_date=from_date),
-    'nubank': Nubank(args.output, conf_account, from_date=from_date),
-    'qif': QIF(args.output, conf_account, from_date=from_date)
+    'itau': Itau(args.output_file, conf, from_date=from_date),
+    'nubank': Nubank(args.output_file, conf, from_date=from_date),
+    'qif': QIF(args.output_file, conf, from_date=from_date)
 }
 
-BANK = LIST_BANKS[args.account]
-BANK.read_file(args.input_file)
-BANK.write_entry()
+if input_file:
+    BANK = LIST_BANKS[args.account]
+    BANK.read_file(input_file)
+    BANK.write_entry()
