725bd14fcc667b28bec63cbd9dbd5187b5d4895d
Paulo Flabiano Smorigo <pfsmorigo@gmail.com>
2018-01-23 07:10:50 -0200

    rename.py: now suport glob

--- rename.py
+++ rename.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
 import sys
+import glob
 
 if len(sys.argv) < 4:
     print "Need arguments! %s <file> <old> <new>" % sys.argv[0]
 else:
-    input_file = open(sys.argv[1], 'r').readlines()
-    write_file = open(sys.argv[1], 'w')
-
     old_txt = sys.argv[2]
     new_txt = sys.argv[3]
 
@@ -24,9 +22,13 @@ else:
         old_txt_padded = old_txt
         new_txt_padded = new_txt.ljust(len(new_txt)+len(old_txt)-len(new_txt))
 
-    for line in input_file:
-        if line.endswith(old_txt+"\n"):
-            new_line = line.replace(old_txt, new_txt)
-        else:
-            new_line = line.replace(old_txt_padded, new_txt_padded)
-        write_file.write(new_line)
+    for FILENAME in glob.glob(sys.argv[1]):
+        input_file = open(FILENAME, 'r').readlines()
+        write_file = open(FILENAME, 'w')
+
+        for line in input_file:
+            if line.endswith(old_txt+"\n"):
+                new_line = line.replace(old_txt, new_txt)
+            else:
+                new_line = line.replace(old_txt_padded, new_txt_padded)
+            write_file.write(new_line)
